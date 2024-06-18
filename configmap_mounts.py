5faf52914e7c8b01740269ae0ad7a984d5d260e4
Ozkan Poyrazoglu <ozkan.poyrazoglu@bestcloudfor.me>
2024-06-18 12:32:21 +0300

    exclude namespace added

--- configmap_mounts.py
+++ configmap_mounts.py
@@ -1,32 +1,24 @@
 import logging
 from kubernetes import client
 
-def check_configmap_mounts(v1: client.CoreV1Api):
+def check_configmap_mounts(v1: client.CoreV1Api, exclude_namespaces):
     logger = logging.getLogger(__name__)
     
-    logger.info("Podlar alınıyor...")
-    pods = []
-    pod_list = v1.list_pod_for_all_namespaces()
-    logger.info(f"{len(pod_list.items)} adet pod bulundu.")
-    
+    logger.info("Fetching pods with ConfigMap mounts...")
     configmap_mounts = []
+    pod_list = v1.list_pod_for_all_namespaces()
+    logger.info(f"{len(pod_list.items)} pods found.")
     
     for pod in pod_list.items:
-        for volume in pod.spec.volumes:
-            if volume.config_map:
-                configmap_name = volume.config_map.name
-                mount_paths = [
-                    container_volume_mount.mount_path
-                    for container in pod.spec.containers
-                    for container_volume_mount in container.volume_mounts
-                    if container_volume_mount.name == volume.name
-                ]
-                if mount_paths:
+        if pod.metadata.namespace not in exclude_namespaces:
+            for volume in pod.spec.volumes:
+                if volume.config_map:
+                    mount_paths = [mount.mount_path for mount in pod.spec.containers[0].volume_mounts if mount.name == volume.name]
                     configmap_mounts.append({
                         'pod_name': pod.metadata.name,
                         'namespace': pod.metadata.namespace,
-                        'configmap_name': configmap_name,
-                        'mount_paths': ", ".join(mount_paths)
+                        'configmap_name': volume.config_map.name,
+                        'mount_paths': ', '.join(mount_paths)
                     })
     
     return configmap_mounts
--- network_policies.py
+++ network_policies.py
@@ -1,20 +1,21 @@
 import logging
 from kubernetes import client
 
-def check_network_policies(v1: client.NetworkingV1Api):
+def check_network_policies(v1: client.NetworkingV1Api, exclude_namespaces):
     logger = logging.getLogger(__name__)
     
-    logger.info("NetworkPolicy'ler alınıyor...")
+    logger.info("Fetching Network Policies...")
     network_policies = []
     network_policy_list = v1.list_network_policy_for_all_namespaces()
-    logger.info(f"{len(network_policy_list.items)} adet NetworkPolicy bulundu.")
+    logger.info(f"{len(network_policy_list.items)} Network Policies found.")
     
     for np in network_policy_list.items:
-        network_policies.append({
-            'name': np.metadata.name,
-            'namespace': np.metadata.namespace,
-            'pod_selector': np.spec.pod_selector.match_labels if np.spec.pod_selector else 'None',
-            'policy_types': np.spec.policy_types if np.spec.policy_types else 'None'
-        })
+        if np.metadata.namespace not in exclude_namespaces:
+            network_policies.append({
+                'name': np.metadata.name,
+                'namespace': np.metadata.namespace,
+                'pod_selector': np.spec.pod_selector.match_labels if np.spec.pod_selector else 'None',
+                'policy_types': np.spec.policy_types if np.spec.policy_types else 'None'
+            })
     
     return network_policies
